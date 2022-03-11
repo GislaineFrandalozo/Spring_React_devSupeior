3c208346a8fdb7f78fa20f3358dbd99ed308b477
Gislaine Frandalozo <86322789+GislaineFrandalozo@users.noreply.github.com>
2022-03-10 22:11:06 -0400

    Form layout

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -5,13 +5,15 @@ export default function Navbar() {
     return (
         <header className='dsmovie-navbar'>
             <nav className="container-fluid px-5">
-                <div className='row p-2 align-items-center'>
+                <div className='row p-3 align-items-center'>
                     <div className='col text-light fs-2 fw-bold'>DSMovie</div>
                     <a className='col-auto' href="https://github.com/GislaineFrandalozo">
                         <div className="row gx-2">
-                            <GitHub className='col align-self-center' />
+                            <div className='col align-self-center' style={{ width: '2rem'}} >
+                            <GitHub />
+                            </div>
                             <div className="col d-flex">
-                                <p className='m-2 fs-6 text-light'>/GislaineFrandalozo</p>
+                                <p className='m-2 mt-2 pt-1 fs-6 text-light'>/GislaineFrandalozo</p>
                             </div>
                         </div>
                     </a>
--- frontend/src/pages/form.jsx
+++ frontend/src/pages/form.jsx
@@ -1,7 +1,42 @@
+import { Link } from "react-router-dom"
+
 export default function Form() {
-    return(
-        <div>
-            Form
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+    return (
+        <div class="d-flex justify-content-center m-4"> 
+       
+        <div class="card " style={{ width: '35rem'}}>
+            <img src={movie.image} class="card-img-top" alt="..." />
+            <div class="card-body">
+                <div class="d-flex justify-content-center p-2">
+                <h5 class="card-title fs-5 fw-bolder">{movie.title}</h5>
+                </div>
+                <form className="mb-2"> 
+                <div class="mb-2 mt-2">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label text-secondary">Informe seu email</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label text-secondary">Informe sua avaliação</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1"  />
+                </div>
+                </div>
+                <div  class="d-flex justify-content-center p-1">
+                <button type="submit" class="btn btn-primary col-6">Salvar</button>
+                </div>
+                </form>
+               <Link  class="d-flex justify-content-center p-1" to="/">
+               <button class="btn btn-primary col-6">Cancelar</button>
+               </Link>
+            </div>
+        </div>
         </div>
     )
 }
